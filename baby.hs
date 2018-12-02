4679f6dd38b8fc4a25c4c953fe32412e3f13d3e7
Pedro <phls73376@gmail.com>
2018-12-02 11:35:23 -0300

    if then else

--- baby.hs
+++ baby.hs
@@ -4,3 +4,9 @@ doubleMe x = x * 2
 doubleUs :: [Int] -> [Int]
 doubleUs [] = []
 doubleUs (x:xs) = doubleMe x : doubleUs xs
+
+doubleSmallNumber :: Int -> Int
+doubleSmallNumber x =
+    if x < 5
+        then x * 2
+        else x
